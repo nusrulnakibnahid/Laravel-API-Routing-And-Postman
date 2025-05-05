4df08e70f1103588b55b0c8a72b4643f00840070
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-05-06 01:23:36 +0600

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+                'title' => 'required|string|max:255',
+                'body' => 'required',
+            ]);
+
+
+             if($validator->fails()){
+            return $validator->errors();
+ 
+        }
+        $post = Post::create([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+
+        if(!$post){
+            return "Post not found";
+            
+        }
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $post = Post::find($id);
+        if(!$post){
+            return "Post not found";
+            
+        }
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'body' => 'required',
+        ]);
+
+
+         if($validator->fails()){
+        return $validator->errors();
+
+    }
+    $post = Post::create([
+        'title' => $request->title,
+        'body' => $request->body,
+    ]);
+    }
+
+    
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+
+        if(!$post){
+            return "Post not found";
+            
+        }
+        $post->delete();
+
+        return "Post deleted successfully";
+    }
+}
