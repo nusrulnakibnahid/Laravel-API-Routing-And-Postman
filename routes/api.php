ca0096e871de893c2262abcb532088e7707324f0
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-05-06 02:34:04 +0600

    Add PostController implementations for API v1 and v2 with resource methods

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostController as PostController;
+use App\Http\Controllers\API\V1\PostController as PostControllerV1;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +12,14 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('/posts', PostController::class);
+
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/posts', PostControllerV1::class);
+});
+
+
+// Not Implemented
+Route::prefix('v2')->group(function () {
+    Route::apiResource('/posts', PostController::class);
+});
